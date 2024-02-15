6dcb4506fb6c921359a8345ad72d0d5711476cd1
salmabenhassin <salmabenhssin52@gmail.com>
2024-02-15 17:42:55 +0100

    initial commit

---
+++ src/app/top-widgets/top-widgets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-widgets',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-widgets.component.html',
+  styleUrl: './top-widgets.component.css'
+})
+export class TopWidgetsComponent {
+
+}
